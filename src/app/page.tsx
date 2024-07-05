0c07428d9383a526b6451023accbdadab21ca2e9
Han-park <ssam032000@gmail.com>
2024-07-06 00:32:30 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>hello this is a daejangang website</p>
+      <p>make shit, make happen</p>
+      <p>LFG</p>
     </main>
   );
 }
